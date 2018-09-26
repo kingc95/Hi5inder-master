65fc309203906a569b92df32a5fea16d5ffe8b29
kingc95 <cmacks95@gmail.com>
2018-09-26 01:45:46 -0400

    Styling and Login Screen (Layout Only)

--- app/src/main/java/hi5inder/siefech/com/hi_5inder/MainActivity.java
+++ app/src/main/java/hi5inder/siefech/com/hi_5inder/MainActivity.java
@@ -1,5 +1,6 @@
 package hi5inder.siefech.com.hi_5inder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -82,6 +83,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_manage) {
