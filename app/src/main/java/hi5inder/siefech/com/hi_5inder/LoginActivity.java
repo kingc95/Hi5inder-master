2bd3095fd4ad5b3223d0f2e8e41a4afa130cdbb3
kingc95 <cmacks95@gmail.com>
2018-09-26 22:14:52 -0400

    Login Functionality

--- app/src/main/java/hi5inder/siefech/com/hi_5inder/LoginActivity.java
+++ app/src/main/java/hi5inder/siefech/com/hi_5inder/LoginActivity.java
@@ -1,13 +1,118 @@
 package hi5inder.siefech.com.hi_5inder;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //initialize views
+    private Button loginButton;
+    private EditText emailText;
+    private EditText passText;
+    private TextView signUpText;
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //getting firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the objects getcurrentuser method is not null
+        //means user is already logged in
+        if (firebaseAuth.getCurrentUser() != null) {
+            //close this activity
+            finish();
+            //opening profile activity
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+        //initializing views
+        emailText = (EditText) findViewById(R.id.emailEditText);
+        passText = (EditText) findViewById(R.id.passwordEditText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpText = (TextView) findViewById(R.id.signUpText);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching click listener
+
+        loginButton.setOnClickListener(this);
+        signUpText.setOnClickListener(this);
+    }
+
+    //method for user login
+    private void userLogin(){
+        String email = emailText.getText().toString().trim();
+        String password  = passText.getText().toString().trim();
+
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter valid email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter  password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        progressDialog.setMessage("Signing in Please Wait...");
+        progressDialog.show();
+
+        //logging in the user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        //if the task is successfull
+                        if(task.isSuccessful()){
+                            //start the profile activity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == loginButton){
+            userLogin();
+        }
+
+        else if (v == signUpText){
+            finish();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
     }
 }
